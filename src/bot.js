d325d3a5fb3eafa0e48e8cb9f5c60bb4be461e2c
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-19 19:26:47 +0800

    Add command file parser

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,31 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
 const dotenv = require('dotenv');
+const fs = require('fs');
+const path = require('path');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 dotenv.config();
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+	.readdirSync(commandsPath)
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+		);
+	}
+}
 
 client.once(Events.ClientReady, (c) => {
 	console.log(`${c.user.tag} is ready!`);
