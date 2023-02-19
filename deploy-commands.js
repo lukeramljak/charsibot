c2755c5738b410e33b57adcac4f0d8022541c48d
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-19 19:53:18 +0800

    Remove src directory

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId } = require('../config.json');
+const { clientId, guildId } = require('./config.json');
 const fs = require('fs');
 
 const commands = [];
