3d24ee4b3aeeea4d2fbd1ba64d8facc9f629f50d
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-19 17:07:38 +0800

    It finally works

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -1,3 +1,5 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
 const fs = require("fs");
 
 module.exports = (client) => {
@@ -12,10 +14,21 @@ module.exports = (client) => {
       for (const file of commandFiles) {
         const command = require(`../../commands/${folder}/${file}`);
         commands.set(command.data.name, command);
-        commandArray.push(command, command.data.toJSON());
-        console.log(
-          `Command: ${command.data.name} has been passed through the handler`
-        );
+        commandArray.push(command.data.toJSON());
+      }
+
+      const clientId = "1076717108064432179";
+      const guildId = "1017719808680267836";
+      const rest = new REST({ version: "10" }).setToken(process.env.token);
+      try {
+        console.log("Started refreshing application (/) commands.");
+
+        await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+          body: client.commandArray,
+        });
+        console.log("Successfully reloaded application (/) commands.");
+      } catch (error) {
+        console.error(error);
       }
     }
   };
