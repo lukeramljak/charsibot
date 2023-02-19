9df5197b3ec7e7dc39794209807bb5a50533d448
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-19 19:03:54 +0800

    Start over

--- src/bot.js
+++ src/bot.js
@@ -1,30 +1,11 @@
-require("dotenv").config();
-const { token } = process.env;
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const dotenv = require('dotenv');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const fs = require("fs");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+dotenv.config();
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
+client.once(Events.ClientReady, (c) => {
+	console.log(`${c.user.tag} is ready!`);
 });
 
-client.commands = new Collection();
-client.commandArray = [];
-
-const functionFolders = fs.readdirSync(`./src/functions`);
-for (const folder of functionFolders) {
-  const functionFiles = fs
-    .readdirSync(`./src/functions/${folder}`)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of functionFiles)
-    require(`./functions/${folder}/${file}`)(client);
-}
-
-client.handleEvents();
-client.handleCommands();
-client.login(token);
+client.login(process.env.token);
