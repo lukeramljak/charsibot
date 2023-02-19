70e49e7272b6b69bdb5cee836b5032d0742425d6
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-19 16:23:18 +0800

    Initial setup

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,8 @@
-const fs = require("node:fs");
-const path = require("node:path");
+require("dotenv").config();
+const { token } = process.env;
+
+const fs = require("fs");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("../config.json");
 
 const client = new Client({
   intents: [
@@ -13,32 +14,17 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+client.commandArray = [];
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  client.commands.set(command.data.name, command);
+const functionFolders = fs.readdirSync(`./src/functions`);
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
 }
 
-client.once(Events.ClientReady, () => {
-  console.log("charsibot is ready!");
-});
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = client.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
-  }
-});
-
+client.handleEvents();
+client.handleCommands();
 client.login(token);
--- src/commands/dog.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("dog").setDescription("what is he doing?"),
-  async execute(interaction) {
-    await interaction.reply("What the dog doin'?");
-  },
-};
