d70d2d42a8683a28e534048dd82df172c69f83b2
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-25 15:16:22 +0800

    Starting over again

--- src/bot.js
+++ src/bot.js
@@ -16,31 +16,12 @@ const client = new Client({
 
 client.login(process.env.token);
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(
-			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-		);
-	}
-}
+client.on(Events.ClientReady, (c) => {
+	console.log(`${c.user.username} is ready!`);
+});
 
 client.on('message', (message) => {
 	if (message.content.toLowerCase() === 'egg') {
 		message.channel.send('egg');
 	}
-});
-
-client.once(Events.ClientReady, (c) => {
-	console.log(`${c.user.tag} is ready!`);
 });
