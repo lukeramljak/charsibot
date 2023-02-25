4869635a9cd5dd01c62e0e81d7a5040927bc111a
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-25 13:45:06 +0800

    Remove commands

--- commands/egg.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require('discord.js');
-
-module.exports = {
-	data: new SlashCommandBuilder().setName('egg').setDescription('egg'),
-	async execute(interaction) {
-		await interaction.reply('egg');
-	},
-};
