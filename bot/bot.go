28f67f7bb9a7a99d2931c576b3f291182a2c4cb3
Luke Ramljak <lukeramljak@proton.me>
2025-01-18 14:52:51 +0800

    Tidy up

--- bot/bot.go
+++ bot/bot.go
@@ -4,6 +4,7 @@ import (
 	"charsibot/bot/commands"
 	"charsibot/bot/events"
 	"fmt"
+	"log"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -13,35 +14,15 @@ type Bot struct {
 }
 
 func NewBot(appID string, guildID string, token string) (*Bot, error) {
-	if token == "" {
-		return nil, fmt.Errorf("Bot token is required")
-	}
-
 	session, err := discordgo.New("Bot " + token)
 	if err != nil {
 		return nil, fmt.Errorf("Error creating Discord session: %w", err)
 	}
 
-	for _, handler := range events.MessageHandlers {
-		session.AddHandler(handler)
-	}
-	session.AddHandler(events.GuildMemberRemove)
-	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
-		}
-	})
-
-	fmt.Println("Registering commands...")
-
-	_, err = session.ApplicationCommandBulkOverwrite(appID, guildID, commands.Commands)
-	if err != nil {
-		return nil, fmt.Errorf("Error creating commands: %w", err)
-	}
-
-	fmt.Println("Successfully registered " + fmt.Sprint(len(commands.Commands)) + " commands")
-
 	session.Identify.Intents = discordgo.IntentsGuildMessages | discordgo.IntentsGuildMembers
+
+	addAllHandlers(session)
+	registerCommands(session, appID, guildID)
 
 	return &Bot{Session: session}, nil
 }
@@ -57,7 +38,6 @@ func (b *Bot) Start() error {
 		return fmt.Errorf("Error setting listening status: %w", err)
 	}
 
-	fmt.Println("Ready! Logged in as", b.Session.State.User.Username+". Press CTRL-C to exit.")
 	return nil
 }
 
@@ -66,5 +46,30 @@ func (b *Bot) Close() error {
 	if err != nil {
 		return fmt.Errorf("Error closing Discord session: %w", err)
 	}
+	return nil
+}
+
+func addAllHandlers(s *discordgo.Session) {
+	for _, handler := range events.MessageHandlers {
+		s.AddHandler(handler)
+	}
+	s.AddHandler(events.GuildMemberRemove)
+	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Ready! Logged in as %s. Press CTRL-C to exit.", s.State.User.Username)
+	})
+}
+
+func registerCommands(s *discordgo.Session, appID string, guildID string) error {
+	fmt.Println("Registering commands...")
+	_, err := s.ApplicationCommandBulkOverwrite(appID, guildID, commands.Commands)
+	if err != nil {
+		return fmt.Errorf("Error creating commands: %w", err)
+	}
+	fmt.Println("Successfully registered " + fmt.Sprint(len(commands.Commands)) + " commands")
 	return nil
 }
