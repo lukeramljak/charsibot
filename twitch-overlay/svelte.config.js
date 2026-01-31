deb3558c6da2c4d41cecdfb407b2ede9213a91b4
Luke Ramljak <lukeramljak@proton.me>
2026-01-31 15:12:36 +0800

    chore(overlay): use node adapter

--- twitch-overlay/svelte.config.js
+++ twitch-overlay/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,12 +7,7 @@ const config = {
   // for more information about preprocessors
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: "index.html",
-      strict: true,
-    }),
+    adapter: adapter(),
   },
 };
 
